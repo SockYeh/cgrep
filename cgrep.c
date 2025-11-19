676fe36f9ecd85596842a325375b4ce5f4c3cd12
SockYeh <sahranashfaq@gmail.com>
2025-11-19 07:49:10 +0000

    feat: add flag handling

--- cgrep.c
+++ cgrep.c
@@ -2,26 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <regex.h>
+#include <unistd.h>
 
 #define MAX_LINE_LENGTH 256
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3) {
-        printf("Usage: ./cgrep \"pattern\" file\n");
+    int caseInsensitve = 0;
+    int invertMatch = 0;
+    int option;
+
+    while ((option = getopt(argc, argv, "iv")) != -1) {
+        switch (option) {
+            case 'i': caseInsensitve = 1; break;
+            case 'v': invertMatch = 1; break;
+            default:
+                fprintf(stderr, "Error: Unknown option %c\n", option);
+                return 1;
+        }
+    }
+
+
+    if ((argc - optind) < 2) {
+        fprintf(stderr, "Usage: ./cgrep \"pattern\" [-flags] file\n");
         return 1;
     }
     
     FILE *filePointer;
-    filePointer = fopen(argv[2],"r");
+    filePointer = fopen(argv[optind + 1],"r");
     
     if (filePointer == NULL) {
-        printf("Error: Could not open file.\n");
+        fprintf(stderr, "Error: Could not open file.\n");
+        
         return 1;
     }
 
     regex_t reegex;
-    int reti = regcomp(&reegex,argv[1], REG_EXTENDED);
+    int reti = regcomp(&reegex,argv[optind], REG_EXTENDED);
 
     char line_buffer[MAX_LINE_LENGTH];
     while (fgets(line_buffer, MAX_LINE_LENGTH, filePointer) != NULL) {
@@ -35,8 +52,6 @@ int main(int argc, char *argv[])
         }
         
     }
-
-
 
     fclose(filePointer);
     regfree(&reegex);
